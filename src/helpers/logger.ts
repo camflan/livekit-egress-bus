1e82e70daa1a338c8063a0187bf8addff0ecd3a3
Camron Flanders <me@camronflanders.com>
2025-04-01 16:36:32 -0500

    add prefixing to logs

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,5 +1,41 @@
-import { setDefaultLevel, getLogger } from "loglevel";
+import log from "loglevel";
+import chalk from "chalk";
+import prefix from "loglevel-plugin-prefix";
+import { safeJoin } from "@uplift-ltd/strings";
 
-setDefaultLevel("DEBUG");
+const colors = {
+  TRACE: chalk.magenta,
+  DEBUG: chalk.cyan,
+  INFO: chalk.blue,
+  WARN: chalk.yellow,
+  ERROR: chalk.red,
+} as const;
 
-export { getLogger };
+prefix.reg(log);
+log.enableAll();
+
+prefix.apply(log, {
+  format(level, name, timestamp) {
+    const levelUpper = level.toUpperCase();
+    const color =
+      levelUpper in colors
+        ? colors[levelUpper as keyof typeof colors]
+        : colors.INFO;
+
+    return safeJoin(" ")(
+      timestamp && chalk.white(`[${timestamp}]`),
+      color(level),
+      chalk.green(`${name}: `),
+    );
+  },
+});
+
+prefix.apply(log.getLogger("critical"), {
+  format(level, name, timestamp) {
+    return chalk.red.bold(`[${timestamp}] ${level} ${name}:`);
+  },
+});
+
+log.setDefaultLevel("DEBUG");
+
+export const getLogger = log.getLogger;
