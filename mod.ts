5318ab9a463d296eb6863accd651ac5798e68a15
Camron Flanders <me@camronflanders.com>
2025-04-03 16:12:06 -0500

    export errors

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 export * from "./src/bus.ts";
+export * from "./src/helpers/errors.ts";
 export * from "./src/protobufs.ts";
 export * from "./src/rpc-client.ts";
 export * from "./src/rpc-server.ts";
