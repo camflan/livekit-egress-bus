54bcab78f895c4c963f2fcedfc4be526fa4b4f97
Camron Flanders <me@camronflanders.com>
2025-04-15 15:32:54 -0500

    export more enum/constants

--- src/protobufs.ts
+++ src/protobufs.ts
@@ -1,20 +1,44 @@
 export {
   ClaimRequest,
   ClaimResponse,
-  type MessageFns,
+  Msg,
   Request,
   Response,
-  Msg,
+  type MessageFns,
 } from "./generated/internal.ts";
 
 export {
   EgressInfo,
+  EgressStatus,
   EncodingOptionsPreset,
   ListEgressRequest,
   ListEgressResponse,
   StopEgressRequest,
-  EgressStatus,
 } from "./generated/livekit_egress.ts";
+
+export {
+  AudioCodec,
+  AudioTrackFeature,
+  BackupCodecPolicy,
+  ConnectionQuality,
+  DisabledCodecs,
+  DisconnectReason,
+  ImageCodec,
+  ParticipantInfo,
+  ParticipantInfo_AttributesEntry,
+  ParticipantInfo_Kind,
+  ParticipantInfo_State,
+  ParticipantPermission,
+  ParticipantTracks,
+  ReconnectReason,
+  SimulcastCodecInfo,
+  SipDTMF,
+  SubscriptionError,
+  TrackSource,
+  TrackType,
+  VideoCodec,
+  VideoQuality,
+} from "./generated/livekit_models.ts";
 
 export { StartEgressRequest } from "./generated/rpc/egress";
 export { GetEgressRequest, UpdateMetricsRequest } from "./generated/rpc/io";
